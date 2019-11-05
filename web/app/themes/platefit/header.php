e92cb7afee6050efce9d79c6e0ec00941c1aeca2
Rodrigo Mantica <manticarodrigo@gmail.com>
2019-11-05 17:54:39 -0500

    Added header and footer with dynamic menus.

--- web/app/themes/platefit/header.php
+++ web/app/themes/platefit/header.php
@@ -19,7 +19,7 @@
 			<nav class="px-4 px-3-md px-2-sm container navbar__container" data-component="navbar-container">
 
 				<!-- logo -->
-				<a class="navbar__logo" href="home.html" data-component="navbar-logo">
+				<a class="navbar__logo" href="/" data-component="navbar-logo">
 					<svg><use href="#logo"></use></svg>
 				</a>
 
@@ -38,12 +38,16 @@
 
 				<!-- menu -->
 				<ul class="pl-0 list-unstyled row between navbar__menu">
-					<li><a href="../styleguide/index.html">Style Guide</a></li>
-					<li><a href="trainers.html">The Team</a></li>
-					<li><a href="locations.html">Locations</a></li>
-					<li><a href="community.html">Community</a></li>
-					<li><a href="press.html">Press</a></li>
-					<li><a class="btn" href="#schedule">Reserve a Class</a></li>
+					<?php
+						$menu_items = get_nav_menu_items_by_location('primary_navigation');
+
+						foreach ( $menu_items as $item ) {
+
+							echo '<li><a href="'.$item->url.'" title="'.$item->title.'">'.$item->title.'</a></li>';
+					
+						}
+					?>
+					<li><a class="btn" href="/schedule">Reserve a Class</a></li>
 				</ul>
 
 			</nav>
