1605da5a21b367ada7bb1725fee5f79b9601d51c
Rodrigo Mantica <manticarodrigo@gmail.com>
2019-11-05 20:33:41 -0500

    Moved quotes to own block. Added aria labels.

--- web/app/themes/platefit/blocks/homepage-plate.php
+++ web/app/themes/platefit/blocks/homepage-plate.php
@@ -19,45 +19,7 @@
     </div>
 
     <div class="border-box col-2 col-1-lg">
-      <div class="glide" data-component="slider">
-        <div class="glide__track" data-glide-el="track">
-          <ul class="glide__slides">
-          <?php
-            $args = array(
-              'post_type' => 'testimonial',
-              'post_status' => 'publish',
-              'meta_key' => 'author_name',
-              'orderby' => 'rand',
-              'order' => 'DESC',
-              'posts_per_page' => '5',
-            );
-
-            $testimonials = new WP_Query( $args );
-            if ($testimonials->have_posts()) :
-              while($testimonials->have_posts()) : $testimonials->the_post();
-          ?>
-                <li class="glide__slide">
-                  <div class="py-4 px-6 px-5-xl px-4-sm">
-                    <p class="quote-text"><?php echo get_field('quote', get_the_ID()); ?></p>
-                    <h4 class="text-uppercase quote-author">/ <?php echo get_field('author_name', get_the_ID()); ?></h4>
-                  </div>
-                </li>
-          <?php
-              endwhile;
-              wp_reset_postdata();
-            endif;
-          ?>
-          </ul>
-          <div class="glide__arrows" data-glide-el="controls">
-            <button class="btn-slider btn-slider--left" data-glide-dir="&lt;">
-              <svg><use href="#arrow"></use></svg>
-            </button>
-            <button class="btn-slider" data-glide-dir="&gt;">
-              <svg><use href="#arrow"></use></svg>
-            </button>
-          </div>
-        </div>
-      </div>
+      <?php include('quotes.php');  ?>
     </div>
   </section>
 
