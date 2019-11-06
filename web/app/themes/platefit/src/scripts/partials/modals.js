db5ce0a5acc5e4e1407e6ad30a2f0481715b3306
Rodrigo Mantica <manticarodrigo@gmail.com>
2019-11-05 19:49:22 -0500

    Added newsletter modal cookie check with 90 days expiration.

--- web/app/themes/platefit/src/scripts/partials/modals.js
+++ web/app/themes/platefit/src/scripts/partials/modals.js
@@ -1,12 +1,41 @@
 import MicroModal from 'micromodal';
 
+function setCookie(name, value, days) {
+  let expires = '';
+  if (days) {
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+  const nameEQ = name + '=';
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length ; i++) {
+      const c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+const statusCookieName = 'newsletter-seen';
+const statusCookieValue = 'seen';
+
+
 const config = {
   awaitOpenAnimation: true,
   awaitCloseAnimation: true,
+  onClose: () => setCookie(statusCookieName, statusCookieValue, 90),
 };
 
 const onDomReady = () => {
-  if (document.querySelector('#newsletter-modal')) {
+  // check if the modal has already been shown
+  const hasSeenModal = getCookie(statusCookieName) === statusCookieValue;
+
+  if (!hasSeenModal && document.querySelector('#newsletter-modal')) {
     MicroModal.init(config);
     MicroModal.show('newsletter-modal', config);
   }
