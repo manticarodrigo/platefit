97795267254f82769968659b5eba5a859ac642c0
Rodrigo Mantica <manticarodrigo@gmail.com>
2020-01-06 16:43:14 -0600

    Added optional links for mobile apps.

--- web/app/themes/platefit/blocks/mobile-app.php
+++ web/app/themes/platefit/blocks/mobile-app.php
@@ -2,12 +2,16 @@
   <div class="border-box p-6 p-4-lg col-2 col-1-md text-center-md">
     <h3 class="text-uppercase"><?php the_field('title') ?></h3>
     <p class="paragraph"><?php the_field('description') ?></p>
-    <a class="mr-2 mr-0-sm mb-2 btn-img" target="_blank" href="<?php the_field('ios_link') ?>">
-      <img src="/app/themes/platefit/src/assets/jpgs/app-ios.jpg">
-    </a>
-    <a class="mb-2 btn-img" target="_blank" href="<?php the_field('android_link') ?>">
-      <img src="/app/themes/platefit/src/assets/jpgs/app-android.jpg">
-    </a>
+    <?php if (get_field('ios_link')): ?>
+      <a class="mr-2 mr-0-sm mb-2 btn-img" target="_blank" href="<?php the_field('ios_link') ?>">
+        <img src="/app/themes/platefit/src/assets/jpgs/app-ios.jpg">
+      </a>
+    <?php endif; ?>
+    <?php if (get_field('android_link')): ?>
+      <a class="mb-2 btn-img" target="_blank" href="<?php the_field('android_link') ?>">
+        <img src="/app/themes/platefit/src/assets/jpgs/app-android.jpg">
+      </a>
+    <?php endif; ?>
   </div>
   <div class="border-box p-6 p-4-lg col-2 col-1-md text-center">
     <img class="max-width-full" src="<?php the_field('image') ?>">
