bcb2b68055789b1542c29c0cf91f6b167a92f73d
Rodrigo Mantica <manticarodrigo@gmail.com>
2020-04-01 10:21:17 -0400

    Removed hardcoded GTM tags.

--- web/app/themes/platefit/header.php
+++ web/app/themes/platefit/header.php
@@ -19,33 +19,10 @@
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-	<!-- Google Tag Manager -->
-	<script>
-		(function(w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({
-				'gtm.start': new Date().getTime(),
-				event: 'gtm.js'
-			});
-			var f = d.getElementsByTagName(s)[0],
-				j = d.createElement(s),
-				dl = l != 'dataLayer' ? '&l=' + l : '';
-			j.async = true;
-			j.src =
-				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f.parentNode.insertBefore(j, f);
-		})(window, document, 'script', 'dataLayer', 'GTM-T33B74V');
-	</script>
-	<!-- End Google Tag Manager -->
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T33B74V" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
 
 	<!-- svg spritesheet -->
 	<?php print_svgs() ?>
