ffb6cc4c6a3aaaa04e383894491512dcb6606cda
Rodrigo Mantica <manticarodrigo@gmail.com>
2020-04-04 15:53:33 -0400

    Conditional render for left_text_right_insta headline.

--- web/app/themes/platefit/blocks/flexible-content.php
+++ web/app/themes/platefit/blocks/flexible-content.php
@@ -1,23 +1,23 @@
-<?php if (have_rows('content')): ?>
+<?php if (have_rows('content')) : ?>
   <?php while (have_rows('content')) : the_row(); ?>
 
-    <?php if (get_row_layout() == 'spacer'): ?>
+    <?php if (get_row_layout() == 'spacer') : ?>
 
       <section class="p-<?php the_sub_field('spacing'); ?> bg-color-<?php the_sub_field('bg_color'); ?>"></section>
 
-    <?php elseif (get_row_layout() == 'section_intro'): ?>
+    <?php elseif (get_row_layout() == 'section_intro') : ?>
 
       <section class="py-5 px-4 bg-color-light">
         <div class="max-width">
           <h1 class="h2 text-center text-uppercase letter-spacing dark"><?php the_sub_field('title'); ?></h1>
-          <?php if (get_sub_field('description')): ?>
+          <?php if (get_sub_field('description')) : ?>
             <p class="text-lg text-center paragraph"><?php the_sub_field('description'); ?></p>
           <?php endif; ?>
         </div>
       </section>
 
-    
-    <?php elseif (get_row_layout() == 'full_width_text'): ?>
+
+    <?php elseif (get_row_layout() == 'full_width_text') : ?>
 
       <section class="p-4 px-2-md bg-color-light">
         <div class="max-width">
@@ -26,7 +26,7 @@
         </div>
       </section>
 
-    <?php elseif (get_row_layout() == 'title_shoutout_text'): ?>
+    <?php elseif (get_row_layout() == 'title_shoutout_text') : ?>
 
       <section class="px-6 px-4-lg px-2-md py-4 bg-color-light">
         <h3 class="mb-0-md h2 text-center text-uppercase letter-spacing"><?php the_sub_field('title'); ?></h3>
@@ -43,7 +43,8 @@
               </p>
               <a class="btn btn--arrow" href="<?php the_sub_field('link'); ?>">
                 <?php the_sub_field('link_text'); ?>
-                <svg><use href="#arrow" /></svg>
+                <svg>
+                  <use href="#arrow" /></svg>
               </a>
             </div>
           </div>
@@ -51,17 +52,17 @@
 
       </section>
 
-    <?php elseif (get_row_layout() == 'left_text_right_image'): ?>
+    <?php elseif (get_row_layout() == 'left_text_right_image') : ?>
 
       <section class="px-6 px-4-lg px-2-md py-4 relative row bg-color-light">
-          
+
         <div class="border-box p-4 p-2-sm col-2 col-1-lg">
           <div class="text-center-lg">
-              <?php if (get_sub_field('subtitle')): ?>
-                <p class="h6"><?php the_sub_field('subtitle'); ?></p>
-              <?php endif; ?>
-              <p class="my-0 h3 text-uppercase"><?php the_sub_field('title'); ?></p>
-              <?php the_sub_field('content'); ?>
+            <?php if (get_sub_field('subtitle')) : ?>
+              <p class="h6"><?php the_sub_field('subtitle'); ?></p>
+            <?php endif; ?>
+            <p class="my-0 h3 text-uppercase"><?php the_sub_field('title'); ?></p>
+            <?php the_sub_field('content'); ?>
           </div>
         </div>
 
@@ -71,46 +72,47 @@
 
       </section>
 
-    <?php elseif (get_row_layout() == 'left_slider_right_text'): ?>
+    <?php elseif (get_row_layout() == 'left_slider_right_text') : ?>
 
       <section class="px-6 px-4-lg px-2-md py-4 bg-color-light">
         <article class="relative row">
-          
+
           <div class="border-box p-4 p-2-sm col-2 col-1-lg row center">
             <?php
-              $peek_size = 100;
-              $faded = true;
-              include(locate_template( 'components/slider-images.php', false, false));
+            $peek_size = 100;
+            $faded = true;
+            include(locate_template('components/slider-images.php', false, false));
             ?>
           </div>
 
           <div class="border-box p-4 p-2-sm col-2 col-1-lg">
             <div class="text-center-lg">
-                <?php if (get_sub_field('subtitle')): ?>
-                  <p class="h6"><?php the_sub_field('subtitle'); ?></p>
-                <?php endif; ?>
+              <?php if (get_sub_field('subtitle')) : ?>
+                <p class="h6"><?php the_sub_field('subtitle'); ?></p>
+              <?php endif; ?>
 
-                <p class="my-0 h3 text-uppercase"><?php the_sub_field('title'); ?></p>
+              <p class="my-0 h3 text-uppercase"><?php the_sub_field('title'); ?></p>
 
-                <?php the_sub_field('content'); ?>
+              <?php the_sub_field('content'); ?>
 
-                <?php if (get_sub_field('button_text') && get_sub_field('button_link')): ?>
-                  <a class="btn btn--arrow" href="<?php the_sub_field('button_link'); ?>">
-                    <?php the_sub_field('button_text'); ?>
-                    <svg><use href="#arrow" /></svg>
-                  </a>
-                <?php endif; ?>
+              <?php if (get_sub_field('button_text') && get_sub_field('button_link')) : ?>
+                <a class="btn btn--arrow" href="<?php the_sub_field('button_link'); ?>">
+                  <?php the_sub_field('button_text'); ?>
+                  <svg>
+                    <use href="#arrow" /></svg>
+                </a>
+              <?php endif; ?>
             </div>
           </div>
 
         </article>
       </section>
 
-    <?php elseif (get_row_layout() == 'image_link_banner'): ?>
+    <?php elseif (get_row_layout() == 'image_link_banner') : ?>
 
       <section class="bg-cover banner banner--opaque" style="background-image: url(<?php the_sub_field('background') ?>);" data-parallax="cover">
         <div class="p-6 px-4-lg row around">
-          <?php if (have_rows('links')): ?>
+          <?php if (have_rows('links')) : ?>
             <?php while (have_rows('links')) : the_row(); ?>
               <a class="col-6 col-3-lg col-1-md mb-4-md row center align-center hover-translucent" target="_blank" href="<?php the_sub_field('url'); ?><?php the_sub_field('file'); ?>"><img class="press-img" src="<?php the_sub_field('image'); ?>" /></a>
             <?php endwhile; ?>
@@ -118,7 +120,7 @@
         </div>
       </section>
 
-    <?php elseif (get_row_layout() == 'image_text_image'): ?>
+    <?php elseif (get_row_layout() == 'image_text_image') : ?>
 
 
       <section class="px-4 px-2-md py-6 py-4-md bg-color-white row">
@@ -133,7 +135,8 @@
           <p class="paragraph"><?php the_sub_field('description'); ?></p>
           <a class="btn btn--arrow" href="<?php the_sub_field('button_url'); ?>">
             <?php the_sub_field('button_text'); ?>
-            <svg><use href="#arrow" /></svg>
+            <svg>
+              <use href="#arrow" /></svg>
           </a>
         </div>
 
@@ -142,11 +145,13 @@
         </div>
 
       </section>
-    
-    <?php elseif (get_row_layout() == 'left_text_right_insta'): ?>
-      
+
+    <?php elseif (get_row_layout() == 'left_text_right_insta') : ?>
+
       <section class="px-6 px-4-lg px-2-md px-0-sm py-6 py-4-md bg-color-light">
-        <p class="p-3 h2 text-center text-uppercase"><?php the_sub_field('headline') ?></p>
+        <?php if (get_sub_field('headline')) : ?>
+          <p class="p-3 h2 text-center text-uppercase"><?php the_sub_field('headline') ?></p>
+        <?php endif; ?>
 
         <div class="text-center-xl row">
 
@@ -156,7 +161,8 @@
             <p class="paragraph"><?php the_sub_field('description') ?></p>
             <a class="btn btn--arrow" href="<?php the_sub_field('button_url') ?>">
               <?php the_sub_field('button_text') ?>
-              <svg><use href="#arrow" /></svg>
+              <svg>
+                <use href="#arrow" /></svg>
             </a>
           </div>
 
