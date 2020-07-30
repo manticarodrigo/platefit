ebf64e9ca1e699ea583aaa510f273d79ad53c658
Reid Carver <reid@ipstudio.co>
2020-07-29 22:10:30 -0700

    Mariana Tek Page Template

---
+++ web/app/themes/platefit/page-marianatek.php
@@ -0,0 +1,46 @@
+<?php
+/* Template Name */
+get_header();
+?>
+
+	<main class="px-4 px-3-md px-2-sm overflow-hidden relative container" role="main" aria-label="Main content">
+
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+			<?php the_content(); ?>
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+	</main>
+
+<script>
+(function() {
+// Set tenant name here
+var TENANT_NAME = 'platefit';
+var d = document;
+var sA = ['polyfills', 'js'];
+for (var i = 0; i < sA.length; i++) { var s = d.createElement('script');
+s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s); }
+})();
+ </script>
+ <noscript>
+     Please enable JavaScript to view the
+<a href="https://marianatek.com/?ref_noscript" rel="nofollow"> Web Integrations by Mariana Tek.
+     </a>
+ </noscript>
+
+<?php get_footer(); ?>
