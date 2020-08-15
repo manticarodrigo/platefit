ee3aeb3ebf7fbc1b4f88a568e28f3b7e9628a489
Reid Carver <reid@ipstudio.co>
2020-08-15 05:33:08 -0700

    acf video slide 2

--- web/app/themes/platefit/blocks/video-slider.php
+++ web/app/themes/platefit/blocks/video-slider.php
@@ -167,3 +167,75 @@ acf_add_local_field_group(array(
 ));
 
 endif;
+
+
+/**
+ * Testimonial Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// Create id attribute allowing for custom "anchor" value.
+$id = 'videoslider-' . $block['id'];
+if( !empty($block['anchor']) ) {
+    $id = $block['anchor'];
+}
+
+// Create class attribute allowing for custom "className" and "align" values.
+$className = 'videoslider';
+if( !empty($block['className']) ) {
+    $className .= ' ' . $block['className'];
+}
+if( !empty($block['align']) ) {
+    $className .= ' align' . $block['align'];
+}
+
+// Load values and assign defaults.
+$video_slides = get_field('video_slides');
+
+?>
+<div id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
+
+<?php if( $video_slides ) { ?>
+<div class="owl-carousel owl-theme video-slider-custom">
+  <?php foreach( $video_slides as $slide ) { ?>
+
+    <div class="item">
+      <div class="slider-container">
+        <div class="slider-item">
+              <div class="col-left-slide">
+                <div class="slider-image">
+                  <img src="<?php echo $slide['image']; ?>">
+                </div>
+                <div class="slider-content">
+                        <h1 class="hero__title"><?php echo $slide['title']; ?></h1>
+                        <?php if($slide['description']): ?>
+                        <div class="description"><?php echo $slide['description']; ?></div>
+                        <?php endif; ?>
+                        <?php if($slide['button_text']): ?>
+                          <a class="btn btn--arrow" href="<?php echo $slide['button_url']; ?>"><?php echo $slide['button_text']; ?><svg><use href="#arrow"></use></svg>
+                          </a>
+                        <?php endif; ?>
+                  </div>
+              </div>
+              <div class="col-right-slide">
+                  <?php if($slide['video']): ?>
+                    <video autoplay muted loop>                        
+                      <source src="<?php echo $slide['video']; ?>" type=video/mp4>
+                    </video>
+                  <?php else: ?>
+                    <img src="<?php echo $slide['image']; ?>">
+                  <?php endif; ?>
+              </div>
+        </div>        
+      </div>
+    </div>
+    <?php } ?>
+
+    
+</div>
+<?php } ?>
+</div>
